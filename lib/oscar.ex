dbfbbf8fc81ca14999409255dc70392411b9e3fe
Mohamad El-Husseini <husseini.mel@gmail.com>
2015-10-05 09:57:01 -0300

    adds module doc for main lib module

--- lib/oscar.ex
+++ lib/oscar.ex
@@ -1,2 +1,11 @@
 defmodule Oscar do
+  @moduledoc ~S"""
+  This will include usage instructions for `Oscar`.
+
+  Shortcut functions that bridge `Oscar.Tokeniser` and `Oscar.Counter`.
+
+  ## Examples
+
+    iex> Oscar.tokeniser.tokenise("The Prophet") |> Oscar.counter.token_count
+  """
 end
